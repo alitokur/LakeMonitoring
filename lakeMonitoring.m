3bc832b86a891bb9bdeb884374470a57b8fea68c
alitokur <alitokurr@gmail.com>
2019-11-11 19:50:38 +0300

    the little unicorn walking around

--- lakeMonitoring.m
+++ lakeMonitoring.m
@@ -1,3 +1,12 @@
 % Lake Monitoring
 % Ali Tokur   
 % Computer Engineer - alitokurr@gmail.com 
+% Preparing the Dataset
+
+B = double(imread("T33PVQ_20191105T092151_B04_60m.jp2"));
+G = double(imread("T33PVQ_20191105T092151_B03_60m.jp2"));
+NIR = double(imread("T33PVQ_20191105T092151_B8A_60m.jp2"));
+MIR = double(imread("T33PVQ_20191105T092151_B11_60m.jp2")); 
+
+% first index method
+NDPI = (MIR-G)./(MIR+G); 
