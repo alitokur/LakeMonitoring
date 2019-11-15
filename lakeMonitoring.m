25bc12756210ae037962cbade49c30945a8c05f6
alitokur <alitokurr@gmail.com>
2019-11-15 14:12:24 +0300

    mm guys we need much more focus

--- lakeMonitoring.m
+++ lakeMonitoring.m
@@ -37,22 +37,22 @@ end
 
 %to write the classes to file.
 
-% f1 = fopen('rgbFeaturesForWater.txt','w+');
-% f2 = fopen('rgbFeaturesForLand.txt','w+');
-% fprintf(f1,'# x     y   R   G  B');
+f1 = fopen('rgbFeaturesForWater.txt','w+');
+f2 = fopen('rgbFeaturesForLand.txt','w+');
+% fprintf(f1,'#R   G  B');
 
 % counter_1=0;
 % counter_2=0;
 % for ii = 1:size(TCI,1)
 %     for jj=1:size(TCI,1)
 %         if (taggedImage(ii,jj)==1)
-%         fprintf(f1,'%d %d %d %d %d %d',counter_1,ii,jj,TCI(ii,jj,1),TCI(ii,jj,2),TCI(ii,jj,3));
+%         fprintf(f1,'%d %d %d',TCI(ii,jj,1),TCI(ii,jj,2),TCI(ii,jj,3));
 %         fprintf(f1,'\n');
-%         counter_1 = counter_1+1;
+% %         counter_1 = counter_1+1;
 %         else
-%         fprintf(f2,'%d %d %d %d %d %d',counter_2,ii,jj,TCI(ii,jj,1),TCI(ii,jj,2),TCI(ii,jj,3));
+%         fprintf(f2,'%d %d %d',TCI(ii,jj,1),TCI(ii,jj,2),TCI(ii,jj,3));
 %         fprintf(f2,'\n');
-%         counter_2 = counter_2+1;
+% %         counter_2 = counter_2+1;
 %         end
 %     end
 % end
@@ -89,16 +89,16 @@ end
 data2(ii,1) = B(ii,5);
 data2(ii,2) = B(ii,6);
 end
-
-figure;
-plot(data1(:,1),data1(:,2),'r.','MarkerSize',10)
-hold on
-plot(data2(:,1),data2(:,2),'b.','MarkerSize',10)
-ezpolar(@(x)1);
-ezpolar(@(x)2);
-axis equal
-hold off
-
+% 
+% figure;
+% plot(data1(:,1),data1(:,2),'r.','MarkerSize',10)
+% hold on
+% plot(data2(:,1),data2(:,2),'b.','MarkerSize',10)
+% % ezpolar(@(x)1);
+% % ezpolar(@(x)2);
+% axis equal
+% hold off
+% 
 % data3 = [data1;data2];
 % theclass = ones(200,1);
 % theclass(1:100) = -1;
@@ -106,23 +106,23 @@ hold off
 % %Train the SVM Classifier
 % cl = fitcsvm(data3,theclass,'KernelFunction','rbf',...
 %     'BoxConstraint',Inf,'ClassNames',[-1,1]);
-
+% 
 % % Predict scores over the grid
 % d = 0.02;
 % [x1Grid,x2Grid] = meshgrid(min(data3(:,1)):d:max(data3(:,1)),...
 %     min(data3(:,2)):d:max(data3(:,2)));
 % xGrid = [x1Grid(:),x2Grid(:)];
 % [~,scores] = predict(cl,xGrid);
-
+% 
 % % Plot the data and the decision boundary
 % figure;
 % h(1:2) = gscatter(data3(:,1),data3(:,2),theclass,'rb','.');
 % hold on
-% ezpolar(@(x)1);
-% h(3) = plot(data3(cl.IsSupportVector,1),data3(cl.IsSupportVector,2),'ko');
-% contour(x1Grid,x2Grid,reshape(scores(:,2),size(x1Grid)),[0 0],'k');
-% legend(h,{'-1','+1','Support Vectors'});
-% axis equal
-% hold off
+% % ezpolar(@(x)1);
+% % h(3) = plot(data3(cl.IsSupportVector,1),data3(cl.IsSupportVector,2),'ko');
+% % contour(x1Grid,x2Grid,reshape(scores(:,2),size(x1Grid)),[0 0],'k');
+% % legend(h,{'-1','+1','Support Vectors'});
+% % axis equal
+% % hold off
 
 
