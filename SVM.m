ea0ee54bf70a859aa5f8b1f1cdbda832d5656500
alitokur <alitokurr@gmail.com>
2019-11-30 22:20:08 +0300

    we have a problem houston!

--- SVM.m
+++ SVM.m
@@ -2,32 +2,35 @@
 %%
 file_1 = fopen('features.txt','r');
 formatSpec = '%d %d %d';
-sizeA = [3 600];
+sizeA = [3 3721];
 A = fscanf(file_1,formatSpec,sizeA);
 A=A';
 %%
-file_2 = fopen('class.txt','r');
+file_2 = fopen('allClass.txt','r');
 formatSpec = '%d %d %d';
-sizeB = [1 600];
+sizeB = [1 3721];
 B = fscanf(file_2,formatSpec,sizeB);
 B=B';
 
+
+%%
 file_3 = fopen('mark.txt','r');
 formatSpec = '%d %d %d';
 sizeC = [3 3348900];
 C = fscanf(file_3,formatSpec,sizeC);
 C=C';
 
+%%
 X = A;
 y = B;
 %%
 %80 : %20
-rand_num = randperm(600);
-X_train = X(rand_num(1:480),:);
-y_train = y(rand_num(1:480),:);
+rand_num = randperm(3721);
+X_train = X(rand_num(1:3000),:);
+y_train = y(rand_num(1:3000),:);
 
-X_test = X(rand_num(481:end),:);
-y_test = y(rand_num(481:end),:);
+X_test = X(rand_num(3001:end),:);
+y_test = y(rand_num(3001:end),:);
 
 %%
 c = cvpartition(y_train,'k',5);
@@ -36,14 +39,14 @@ c = cvpartition(y_train,'k',5);
 Md1 = fitcsvm(X_train,y_train,'KernelFunction','rbf','OptimizeHyperparameters','auto',...
       'HyperparameterOptimizationOptions',struct('AcquisitionFunctionName',...
       'expected-improvement-plus','ShowPlots',true));
-%%  
+  
 test_accuracy_for_iter = sum((predict(Md1,X_test) == y_test))/length(y_test)*100; 
 
-%%
+%
 % figure;
 % hgscatter = gscatter(X_train(:,1),X_train(:,2),y_train);
 % hold on;
-% h_sv=plot(Md1.SupportVectors(:,1),Md1.SupportVectors(:,2),'ko','markersize',7-4);
+% h_sv=plot(Md1.SupportVectors(:,1),Md1.SupportVectors(:,2),'ko','markersize',4);
 
 %%
 % gscatter(X_test,X_test,y_test,'rb','xx')
