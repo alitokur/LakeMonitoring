5101eedbced9181c63621d3a4dc8d78b81ecde16
alitokur <alitokurr@gmail.com>
2020-01-05 13:33:56 +0300

    new models :metal:

--- allModels.m
+++ allModels.m
@@ -1,16 +1,22 @@
 T = readtable('data.txt');
+%%
 M = readtable('mark.txt');
+%%
 C = readtable('Labels.txt');
 C = table2cell(C);
-%%Ensemble
+%% Ensemble
 [baggedTreesFit, baggedTreesAC,img_1] = predictionForModel(baggedTrees,M,C);
 [boosteedTreesFit, boostedTreesAC,img_2] = predictionForModel(boostedTrees,M,C);
 [rusBoostedTreesFit, rusBoostedTreesAC,img_3] = predictionForModel(rusBoostedTrees,M,C);
 [subspaceDiscriminantFit, subspaceDiscriminantAC,img_4] = predictionForModel(subspaceDiscriminant,M,C);
 [subspaceKnnFit, subspaceKnnAC,img_5] = predictionForModel(subspaceKnn,M,C);
 [optimizableEnsembleFit, optimizableEnsembleAC,img_6] = predictionForModel(optimizableEnsemble,M,C);
-%%SVM
-
+%% KNN
+[fineKNNFit, fineKnnAC,img_7] = predictionForModel(fineKNN,M,C);
+[mediumKNNFit, mediumKnnAC,img_8] = predictionForModel(mediumKNN,M,C);
+[cosineKNNFit, cosineKnnAC,img_9] = predictionForModel(cosineKNN,M,C); %%not completed
+[cubicKNNFit, cubicKnnAC,img_10] = predictionForModel(cubicKNN,M,C);
+[weightedKNNFit, weightedKNNAC,img_11] = predictionForModel(weightedKNN,M,C);
 
 
 
