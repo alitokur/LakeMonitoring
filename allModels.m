6912d59e32abe9e9ecb351d854fdde3b1481cbf4
alitokur <alitokurr@gmail.com>
2020-01-05 11:40:25 +0300

    svm s coming :metal:

--- allModels.m
+++ allModels.m
@@ -2,6 +2,15 @@ T = readtable('data.txt');
 M = readtable('mark.txt');
 C = readtable('Labels.txt');
 C = table2cell(C);
-[baggedTreesFit, baggedTreesAC,img] = predictionForModel(baggedTrees,M,C);
+%%Ensemble
+[baggedTreesFit, baggedTreesAC,img_1] = predictionForModel(baggedTrees,M,C);
+[boosteedTreesFit, boostedTreesAC,img_2] = predictionForModel(boostedTrees,M,C);
+[rusBoostedTreesFit, rusBoostedTreesAC,img_3] = predictionForModel(rusBoostedTrees,M,C);
+[subspaceDiscriminantFit, subspaceDiscriminantAC,img_4] = predictionForModel(subspaceDiscriminant,M,C);
+[subspaceKnnFit, subspaceKnnAC,img_5] = predictionForModel(subspaceKnn,M,C);
+[optimizableEnsembleFit, optimizableEnsembleAC,img_6] = predictionForModel(optimizableEnsemble,M,C);
+%%SVM
+
+
 
 
